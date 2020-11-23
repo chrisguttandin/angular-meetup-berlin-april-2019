06677af0e639f2ca624ed31bc80481d4377d15d5
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-11-23 02:10:25 +0100

    test: update e2e tests

--- test/e2e/home.po.ts
+++ test/e2e/home.po.ts
@@ -1,17 +1,16 @@
 import { env } from 'process';
 import { browser, by, element } from 'protractor';
-import { promise } from 'selenium-webdriver'; // tslint:disable-line:no-implicit-dependencies
 
 export class HomePage {
-    public getHeadline(): promise.Promise<string> {
+    public async getHeadline(): Promise<string> {
         return element(by.css('amb-app h1')).getText();
     }
 
-    public getSubHeadline(): promise.Promise<string> {
+    public async getSubHeadline(): Promise<string> {
         return element(by.css('amb-app h2')).getText();
     }
 
-    public navigateTo(): promise.Promise<any> {
+    public async navigateTo(): Promise<any> {
         return browser.get(env.IS_SMOKE_TEST === 'true' ? '/angular-meetup-berlin-april-2019' : '/');
     }
 }
