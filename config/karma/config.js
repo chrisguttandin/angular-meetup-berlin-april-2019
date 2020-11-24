2544f0486b4e755f354cc6aec992cee56e7da23e
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-11-24 06:01:17 +0100

    build(config): update karma config

--- config/karma/config.js
+++ config/karma/config.js
@@ -10,7 +10,7 @@ module.exports = (config) => {
         },
 
         coverageReporter: {
-            dir: join(__dirname, '../../coverage'),
+            dir: join(__dirname, '../../coverage/angular-meetup-berlin-april-2019'),
             reporters: [{ type: 'html' }, { type: 'text-summary' }],
             subdir: '.'
         },
