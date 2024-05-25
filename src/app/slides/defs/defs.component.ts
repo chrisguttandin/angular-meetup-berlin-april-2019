a9c99ca192a26017775a76b7085c3b6e03aee350
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-05-25 22:59:53 +0200

    fix: use id without URL to reference gradient

--- src/app/slides/defs/defs.component.ts
+++ src/app/slides/defs/defs.component.ts
@@ -1,20 +1,10 @@
-import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
-    providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }],
     selector: 'amb-defs',
     standalone: true,
     styleUrls: ['./defs.component.scss'],
     templateUrl: './defs.component.html'
 })
-export class DefsComponent {
-    public pastaPath: string;
-
-    constructor(location: Location) {
-        const slicedPath = location.path().slice(1);
-
-        this.pastaPath = `url(${slicedPath}#pasta)`;
-    }
-}
+export class DefsComponent {}
